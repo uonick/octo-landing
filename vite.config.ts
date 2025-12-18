a943991075e78e887497505df1dfc747a4143823
uonick <hello@uonick.com>
2025-12-18 22:22:25 +0500

    Enhance Vite configuration by adding minification options to remove comments and collapse whitespace, and include additional file extensions for module resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { ViteMinifyPlugin } from 'vite-plugin-minify'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteMinifyPlugin()],
+  plugins: [
+    vue(),
+    ViteMinifyPlugin({
+      removeComments: true,
+      collapseWhitespace: true,
+    }),
+  ],
   base: '/',
   define: {
     __VUE_OPTIONS_API__: true,
@@ -17,9 +22,11 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
   build: {
     outDir: './dist',
     emptyOutDir: true,
+    sourcemap: false,
   },
 })
