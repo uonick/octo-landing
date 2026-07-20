c3cb2bf25ba3baa2af5b45ce55fb7cbcd2f9c699
uonick <hello@uonick.com>
2026-07-21 03:24:17 +0500

    Remove .env file and related environment variable; refactor download links to use a constant instead of environment variable. Update styles in hero and cta sections for consistency.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-  readonly VITE_GITHUB_DOWNLOAD_LINK: string
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
--- src/lib/download-links.ts
+++ src/lib/download-links.ts
@@ -1,18 +1,18 @@
 import packageJson from '../../package.json'
 
+const GITHUB_DOWNLOAD_LINK =
+  'https://github.com/uonick/octo-workspace/releases/download/%version%/OctoWorkspace-%version%-install.%ext%'
+
 type DownloadLinks = {
   mac: string
   linux: string
   windows: string
 }
 
-export const getDownloadLinks = (): DownloadLinks => {
-  const github = import.meta.env.VITE_GITHUB_DOWNLOAD_LINK
-  return {
-    mac: github.replaceAll('%version%', packageJson.version).replace('%ext%', 'dmg'),
-    linux: github.replaceAll('%version%', packageJson.version).replace('%ext%', 'deb'),
-    windows: github.replaceAll('%version%', packageJson.version).replace('%ext%', 'exe'),
-  }
-}
+export const getDownloadLinks = (): DownloadLinks => ({
+  mac: GITHUB_DOWNLOAD_LINK.replaceAll('%version%', packageJson.version).replace('%ext%', 'dmg'),
+  linux: GITHUB_DOWNLOAD_LINK.replaceAll('%version%', packageJson.version).replace('%ext%', 'deb'),
+  windows: GITHUB_DOWNLOAD_LINK.replaceAll('%version%', packageJson.version).replace('%ext%', 'exe'),
+})
 
 export const getVersion = (): string => packageJson.version
